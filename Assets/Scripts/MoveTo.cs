f3881b3a0c52ce424fd9ae7a97f4026440425e1f
Owen Harris <owenllharris@gmail.com>
2014-02-20 14:07:11 +0000

    added a nav mesh script
    Signed-off-by: Owen Harris <owenllharris@gmail.com>

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	private NavMeshAgent nma;
+
+	public GameObject Target;
+	public float Speed = 1f;
+
+	public bool MoveToClick;
+
+	// Use this for initialization
+	void Start () 
+	{
+		nma = gameObject.AddComponent<NavMeshAgent>();
+		nma.speed = Speed;
+
+		if ( Target != null )
+			nma.destination = Target.transform.position;
+
+		if(MoveToClick)
+			StartCoroutine(DetectClicks());
+	}
+
+	IEnumerator DetectClicks()
+	{
+		while(true)
+		{
+			if( Input.GetMouseButtonDown(0) )
+			{
+				RaycastHit hit;
+				if( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100f ) )
+				{
+					if(hit.collider.gameObject.isStatic)
+						nma.destination = hit.point;
+				}
+			}
+
+
+			yield return new WaitForFixedUpdate();
+		}
+
+	}
+
+
+
+
+	void SetTarget(GameObject target)
+	{
+		Target = target;
+		nma.destination = Target.transform.position;
+	}
+}
